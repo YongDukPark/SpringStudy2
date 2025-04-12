d56a1bd01b0f304428b6bcf11b52f5b453ed6f90
ParkYoungDuk <dydejrdms@naver.com>
2025-04-12 12:49:13 +0900

    기존 코드 내용 주석추가

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -13,13 +13,15 @@ public class MemberApp {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
 
-        SpringApplication.run(CoreApplication.class, args);
+        //SpringApplication.run(CoreApplication.class, args);
 
         // 스프링은 모든게 애플리케이션 컨텍스트라는 것응로 시작을 한다.
         // 이를 스프링 컨테이너라고 보면 된다.
         // 아래와 같은 형식으로 Appconfig를 파라미터로 넣어주면
         // AppConfig안에 있는 Bean들을 스프링 컨테이너에 생성된 객체들을 등록하고 관리를 해준다.
         // 그럼 각 빈의 이름은 매서드 이름으로 등록이 된다.
+
+        // SpringApplication.run 없이도 스프링의 핵심기능을 사용할수 있게 해준다.
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         //이와같이 빈의 이름을 통해 가져온다.
