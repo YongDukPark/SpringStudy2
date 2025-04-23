cceb0e668abddfaf39b59c10863806c6315ee4e7
ParkYoungDuk <dydejrdms@naver.com>
2025-04-23 16:13:32 +0900

    Singleton 패턴 무상태 예시코드

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -1,14 +1,15 @@
 package hello.core.singleton;
 
 public class StatefulService {
-    private int price;
+    //private int price;
 
-    public void order(String name, int price){
+    public int order(String name, int price){
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; //이부분에서 문제가 발생한다.
+        //this.price = price; //이부분에서 문제가 발생한다.
+        return price;   // 상태유지를 하면 안된다.
     }
 
-    public int getPrice(){
-        return price;
-    }
+//    public int getPrice(){
+//        return price;
+//    }
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -17,17 +17,17 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA : A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadA : B사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //TjreadA : 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
+//        int price = statefulService1.getPrice();
         // 여기서 2만원이 나오게 된다 왜냐하면 Order method에서 전역변수의 값을 지정하기 때문이다.
         // 인스턴스가 1개인 상태에서 진행을 하기에 userB가 주문한 금액이 이후 나오게 되는거다.
-        System.out.println("price = " + price);
+        System.out.println("price = " + userAPrice);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
