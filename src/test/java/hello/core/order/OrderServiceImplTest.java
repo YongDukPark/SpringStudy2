4c4efcbcc9a255d7f9aa70743b5e0048ee5e55de
ParkYoungDuk <dydejrdms@naver.com>
2025-05-08 12:40:49 +0900

    생성자 주입 기반 테스트 예시 코드

---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    // 생성자 주입을 채택해야 하는 이유
+    // 더미객체 없이 테스트를 진행할 수 있다.
+    @Test
+    void createOrder(){
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
