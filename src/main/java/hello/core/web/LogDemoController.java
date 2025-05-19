e3ca1aa908211754ecb7bbab79e47762dd8daed9
ParkYoungDuk <dydejrdms@naver.com>
2025-05-20 06:34:49 +0900

    request scope 문제 spring Provider를 사용하여 빈 의존성 주입을 필요시점으로 미뤄 해결하는 예제

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-memo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+
+        //주입받은 오브젝트 호출
+        MyLogger myLogger = myLoggerProvider.getObject();
+
         //고객의 요청 URL 가져오기
         String requestURL = request.getRequestURL().toString();
         myLogger.setReqeustURL(requestURL);
